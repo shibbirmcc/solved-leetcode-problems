56e6678571db3d6f5a57a0960604c7ab3ad70f93
Shibbir Ahmed <shibbirmcc@gmail.com>
2022-01-12 20:54:46 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- problems/fibonacci_number/solution.java
+++ problems/fibonacci_number/solution.java
@@ -1,20 +1,13 @@
 class Solution {
-    int[] fib = new int[31];
-    
     public int fib(int n) {
-        fib[0] = 0;
-        fib[1] = 1;
-        fib[2] = 1;
-        if(n <= 0){
-            return 0;
+        int[] f = new int[n+2];
+        f[0] = 0;
+        f[1] = 1;
+        
+        for(int i = 2; i<=n; i++){
+            f[i] = f[i-1] + f[i - 2];
         }
-        if(n > 2 && fib[n] == 0){
-            for(int i=3; i<=n; i++){
-                fib[i] = fib[i-1] + fib[i - 2];
-            }
-        }
-       return fib[n]; 
+        
+        return f[n];
     }
-    
-    
 }
