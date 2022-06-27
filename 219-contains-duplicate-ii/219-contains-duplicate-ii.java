92de7c7cccd9cdbca34e18e8c39d19db41f4eef4
Shibbir Ahmed <shibbirmcc@gmail.com>
2022-06-27 10:55:50 +0200

    Time: 39 ms (52.70%), Space: 94.1 MB (38.67%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -1,21 +1,13 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Map<Integer, TreeSet<Integer>> indices = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            indices.computeIfAbsent(nums[i], f -> new TreeSet()).add(i);
-        }
-        
+        Map<Integer, Integer> lastIndexMap = new HashMap<>();
         for(int i=0; i < nums.length; i++){
-            TreeSet<Integer> numberIndices = indices.get(nums[i]);
-            if(numberIndices.size() > 1){
-                Integer lowestIndex  = numberIndices.higher(i);
-                if(lowestIndex != null && Math.abs(lowestIndex.intValue() - i) <= k ){
-                    return true;
-                }
-                
+            Integer lastIndex = lastIndexMap.get(nums[i]);
+            if(lastIndex != null && Math.abs(i - lastIndex.intValue()) <= k){
+                return true;
             }
+            lastIndexMap.put(nums[i], i);
         }
-        
-      return false;  
+        return false;
     }
 }
